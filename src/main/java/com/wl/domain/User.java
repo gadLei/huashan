c0d94779466a3953ddf8bd1aaa77db2ab2164eef
gadL_glb <xxx_glb@wl.com>
2020-06-05 13:41:20 +0800

    1111

---
+++ src/main/java/com/wl/domain/User.java
@@ -0,0 +1,12 @@
+package com.wl.domain;
+
+/**
+ * @author gadL
+ * @date 2020/6/5 0005-13:38
+ */
+public class User {
+    private String name;
+    private Integer age;
+    private String sex;
+
+}
